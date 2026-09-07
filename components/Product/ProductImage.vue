44374d495ccf0d423969d0afa5778a5b36779767
w <holodnyak@stilsoft.su>
2026-09-07 15:36:27 +0300

    feat: добавлен computed для вычисления текущего изображения

--- components/Product/ProductImage.vue
+++ components/Product/ProductImage.vue
@@ -1,24 +1,40 @@
 <template>
   <v-card class="product-image-card">
-    <v-img :src="product?.imgs?.at(0)" :alt="product?.title" class="rounded-xl">
+    <v-img :src="currentImage" :alt="product?.title" class="rounded-xl">
       <template #placeholder>
         <v-sheet
           class="d-flex align-center justify-center fill-height"
           color="background"
         >
+          <v-progress-circular
+            indeterminate
+            color="primary"
+            size="48"
+          ></v-progress-circular>
         </v-sheet>
       </template>
     </v-img>
   </v-card>
 </template>
+
 <script setup lang="ts">
+import { computed } from 'vue'
 import type { Product } from '~/assets/types/types'
 
 interface IProps {
   product: Product | undefined
   selectedIndex: number
 }
+
 const props = defineProps<IProps>()
+
+const currentImage = computed(() => {
+  if (!props.product?.imgs?.length) {
+    return undefined
+  }
+
+  return props.product.imgs[props.selectedIndex] ?? props.product.imgs[0]
+})
 </script>
 
 <style scoped></style>
