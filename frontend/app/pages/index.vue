293c2c18b0bb0d352d228b1c6a796409e2f51220
w <holodnyak@stilsoft.su>
2026-09-10 16:31:37 +0300

    feat: интеграция хука работы со списком продукции через поиск, фикс отображения пустых товаров

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <HeaderSearch v-if="mobile" class="header-search" />
+  <SearchInput v-if="mobile" v-model="searchQuery" class="header-search" />
+
   <div class="grid">
     <ProductGrid :products />
+
     <v-empty-state
-      v-if="!isLoading && (!products || products.length === 0)"
+      v-if="!isLoading && products.length === 0"
       icon="mdi-file-search-outline"
       title="Товары не найдены"
       text="Попробуйте изменить параметры поиска или фильтра"
@@ -13,27 +15,14 @@
 
 <script setup lang="ts">
 import { useDisplay } from 'vuetify'
-import HeaderSearch from '~/components/AppHeader/HeaderSearch.vue'
+import { useProductSearch } from '~/components/SearchInput/useProductSearch'
 
 const productStore = useProductsStore()
 const { products, isLoading } = storeToRefs(productStore)
 
 const { mobile } = useDisplay()
 
-function waitTimeoutLoading() {
-  setTimeout(() => {
-    if (!products.value.length) isLoading.value = false
-  }, 1500)
-}
-
-onMounted(() => {
-  productStore.getAllProducts()
-  waitTimeoutLoading()
-})
-
-onUnmounted(() => {
-  productStore.cancelLoading()
-})
+const { searchQuery } = useProductSearch()
 </script>
 
 <style scoped lang="scss">
@@ -44,11 +33,11 @@ $offset-top: 0px;
   left: 0;
   right: 0;
   margin: $offset-top auto;
-  width: calc(100%);
+  width: 100%;
   z-index: 1;
 
   backdrop-filter: blur(15px);
-  background: rgba(var(--v-theme-background), 0.6);
+  background: rgb(var(--v-theme-background) / 0.6);
 }
 
 .grid {
